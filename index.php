6d89e90176bd6c2c842d7addec7760c5715440f8
Erich Osterman <300088801@fvtc.edu>
2023-08-29 10:44:23 -0500

    Initial Commit of Erich's homepage

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Erich's Homepage</title>
+    <link type= "text/css" rel="stylesheet" href="css/base.css">
+</head>
+<body>
+    <header>
+        <h1>PHP Class - Erich's Website</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="/">Homepage</a></li>
+            <li> <a href="#">Loop</a> </li>
+            <li> <a href="#">Countdown</a> </li>
+            <li> <a href="#">Other</a> </li>
+        </ul>
+    </nav>
+    <main>
+        <img src="images/IMG_1370_Original.jpg" alt="This is a picture of Erich Osterman">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore hic id minima pariatur similique velit voluptatum! Dicta et fugit inventore placeat tenetur! Architecto autem, cupiditate dicta laboriosam nihil non nulla pariatur rerum sapiente totam. Incidunt minus perspiciatis rem. Et laborum, unde. Accusamus consequatur corporis dignissimos, eaque earum nihil numquam odio officiis quam quibusdam recusandae sunt. Amet aut eius excepturi molestias possimus quam reprehenderit sequi? Alias dolorem dolorum eaque impedit libero quidem, reiciendis. A, accusamus cum deserunt ea et exercitationem explicabo, fugiat illum in labore molestias mollitia odio perferendis quaerat quisquam quo quos ratione repellendus rerum sint soluta temporibus voluptatem voluptates?</p>
+    </main>
+    <footer>
+        <p>&copy; 2023 ErichOsterman.com</p>
+    </footer>
+</body>
+</html>
+
