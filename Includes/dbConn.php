11f1c96350e072786b8ebfb3c5f8cefcd5435363
Erich Osterman <300088801@fvtc.edu>
2023-09-24 14:20:43 -0500

    dicegame added and works well

---
+++ Includes/dbConn.php
@@ -0,0 +1,8 @@
+<?php
+// Make Database connection
+$dsn ='mysql:host=10.6.112.193; dbname=phpclass';
+$username = 'dbuser';
+$password = 'dbdev123';
+$options = array(
+    PDO::ATTR_ERRMODE=> PDO:: ERRMODE_EXCEPTION);
+?>
--- Includes/nav.php
+++ Includes/nav.php
@@ -4,4 +4,5 @@
     <li> <a href="/clock/countdown.php">Countdown</a> </li>
     <li> <a href="/magic8/magic8.php">Magic 8 Ball</a> </li>
     <li> <a href="/dicegame/dicegame.php">Dice Game</a> </li>
+    <li> <a href="/movielist/movielist.php">My Movie List</a> </li>
 </ul>
