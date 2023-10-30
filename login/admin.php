4f619d59551cb7dd00f145b024609d582915733b
Erich Osterman <300088801@fvtc.edu>
2023-10-29 19:52:02 -0500

    User information is pushed to our database if all of the criteria is met

--- login/admin.php
+++ login/admin.php
@@ -15,7 +15,62 @@ if(isset($_POST["submit"]))
     else{
         $FName=$_POST["txtFName"];
     }
-    echo "We are here";
+
+    if(empty($_POST["txtEmail"]))
+    {
+        $errmsg = "Email is required";
+    }
+    else{
+        $Email=$_POST["txtEmail"];
+    }
+
+    if(empty($_POST["txtLoginPassword"]))
+    {
+        $errmsg = "Password is required";
+    }
+    else{
+        $LoginPassword=$_POST["txtLoginPassword"];
+    }
+
+    if($LoginPassword != $_POST["txtPassword2"])
+    {
+        $errmsg = "Passwords do not match!";
+    }
+
+    if(empty($_POST["txtRole"]))
+    {
+        $errmsg = "Role is required";
+    }
+    else{
+        $Role=$_POST["txtRole"];
+    }
+
+    if($errmsg=="") // if no error messages, we can do database work
+    {
+        //Database stuff
+        include '../Includes/dbConn.php';
+        try{
+            $db = new PDO($dsn, $username, $password, $options);
+
+            $sql = $db->prepare("insert into memberLogin (memberName,memberEmail,memberPassword,RoleID,memberKey) Value (:Name,:Email,:Password,:RID,:Key)");
+
+            $sql->bindValue(":Name", $FName);
+            $sql->bindValue(":Email",$Email);
+            $sql->bindValue(":Password",$LoginPassword);
+            $sql->bindValue(":RID",$Role);
+            $sql->bindValue(":Key","XXXXXXXXXXXX");
+            $sql->execute();
+
+        }catch(PDOException $e){
+            $error = $e->getMessage();
+            echo "Error: $error";
+        }
+
+        $FName="";
+        $Email = "";
+        $errmsg= "Member Added to Database";
+    }
+
 
 }
 
@@ -44,11 +99,11 @@ if(isset($_POST["submit"]))
             </tr>
             <tr height="60">
                 <th>Full Name</th>
-                <td><input id="txtFName" name="txtFName" type="text" size="50" required</td>  <!--inline php to insert our values from above into the rows when the user clicks to update-->
+                <td><input id="txtFName" name="txtFName" value="<?=$FName?>" type="text" size="50" required</td>  <!--inline php to insert our values from above into the rows when the user clicks to update-->
             </tr>
             <tr height="60">
                 <th>Email</th>
-                <td><input id="txtEmail" name="txtEmail" type="text" size="50"></td>
+                <td><input id="txtEmail" name="txtEmail" value="<?=$Email?>" type="text" size="50" required></td>
             </tr>
             <tr height="60">
                 <th>Password</th>
