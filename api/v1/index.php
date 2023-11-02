2f8daccadf6902450a6014e50906ee3f8fe7f51c
Erich Osterman <300088801@fvtc.edu>
2023-11-02 11:10:41 -0500

    Added in api methods to add, update, delete, see runners, and see races

--- api/v1/index.php
+++ api/v1/index.php
@@ -11,11 +11,70 @@ require 'Slim/Slim.php';
 $app = new \Slim\Slim(); /*creating a new instance of slim*/
 
 $app->get('/getHello', 'getHello'); /* if a user types in /getHello, it will run a function called getHello*/
+$app->get('/showMember/:MemberName','showMember');
+$app->post('/addMember/:MemberName', 'addMember');
+$app->post('/addJson', 'addJson');
+$app->delete('/delUser:userID', 'delUser');
+
+$app->get('/get_races', 'get_races');
+$app->get('/get_runners/:race_id','get_runners');
+$app->post('/add_runner','add_runner');
+$app->delete('/delete_runner','delete_runner');
+$app->put('/update_runner','update_runner');
 $app->run();
 
 function getHello()
 {
     echo "Hello World";
+}
+function showMember($MemberName)
+{
+    echo "Hello $MemberName";
+}
+function addMember($MemberName)
+{
+    echo "Welcome $MemberName";
+}
+
+function addJson()
+{
+    $request = \Slim\Slim::getInstance()->request();
+    $post_json = json_decode($request->getBody(), TRUE);
+    echo $post_json["fname"];
+}
+
+function delUser($userID)
+{
+    echo "User $userID was Deleted";
+}
+
+function get_races()
+{
+    echo "Getting Races:";
+}
+function get_runners($raceID)
+{
+    echo "The runners for " . $raceID . " are : ";
+}
+function add_runner()
+{
+    $request = \Slim\Slim::getInstance()->request();
+    $post_json = json_decode($request->getBody(), TRUE);
+    echo "Runner has been added. Runner info:";
+    echo $post_json["fname". "lname"."age". "runnerID"];
+}
+function delete_runner()
+{
+    $request = \Slim\Slim::getInstance()->request();
+    $post_json = json_decode($request->getBody(), TRUE);
+    echo $post_json["fname"] . " " . $post_json["lname"] . " has been deleted from the runners list";
+
+}
+function update_runner()
+{
+    $request = \Slim\Slim::getInstance()->request();
+    $post_json = json_decode($request->getBody(), TRUE);
+    echo $post_json["fname"] . " " . $post_json["lname"] . "'s profile has been updated";
 }
 
 ?>
